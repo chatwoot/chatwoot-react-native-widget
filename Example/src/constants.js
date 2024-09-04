d8d0c7ae06ca53793ce907e3e35fcf961f15608d
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-09-04 13:05:40 +0530

    feat: add all the source codes

---
+++ Example/src/constants.js
@@ -0,0 +1,10 @@
+export const WOOT_PREFIX = 'chatwoot-widget:';
+export const POST_MESSAGE_EVENTS = {
+  SET_LOCALE: 'set-locale',
+  SET_CUSTOM_ATTRIBUTES: 'set-custom-attributes',
+  SET_USER: 'set-user',
+  SET_COLOR_SCHEME: 'set-color-scheme',
+};
+export const COLOR_WHITE = '#fff';
+export const BG_COLOR_WHITE = '#FFFF';
+export const BG_COLOR_DARK = '#25292c';
