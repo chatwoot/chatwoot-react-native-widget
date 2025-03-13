5d9b9cf05f2788520c146d0e410c031535a3e647
Ridho Ade Putra <theonlydo@gmail.com>
2025-03-14 03:13:42 +0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ export const WOOT_PREFIX = 'chatwoot-widget:';
 export const POST_MESSAGE_EVENTS = {
   SET_LOCALE: 'set-locale',
   SET_CUSTOM_ATTRIBUTES: 'set-custom-attributes',
+  SET_CONVERSATION_CUSTOM_ATTRIBUTES: 'set-conversation-custom-attributes',
   SET_USER: 'set-user',
   SET_COLOR_SCHEME: 'set-color-scheme',
 };
