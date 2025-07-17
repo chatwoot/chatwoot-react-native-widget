279e84d6ef98d297ac9ced3dc1ecd7020402f4f5
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-07-17 11:26:08 +0530

    Update App.js

--- Example/App.js
+++ Example/App.js
@@ -25,7 +25,7 @@ const App = () => {
     order_id: 1212,
   };
   const websiteToken = '3mxLvj762wfcMvMUphvKsLg1';
-  const baseUrl = 'https://muhsin.chatwoot.dev';
+  const baseUrl = 'https://sdsd.chatwoot.dev';
   const [locale, setLocale] = useState('en');
 
   const clearCookies = async () => {
